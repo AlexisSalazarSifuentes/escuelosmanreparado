5434041b240856e11fe980e9b35242a43c8c78d5
AlexisSalazarSifuentes <100451787+AlexisSalazarSifuentes@users.noreply.github.com>
2022-03-12 15:49:56 -0600

    htmlspecialchar

--- php/v/1/modificar_alumno.php
+++ php/v/1/modificar_alumno.php
@@ -25,30 +25,30 @@ $id_alumno = $row_alumno[0]['id_alumno'];
 	      <div class="col-md-4 px-1">
 	        <div class="form-group">
 	          <label>Nombre</label>
-	          <input type="text" id="nombre_alumno" class="form-control" placeholder="Nombre" value="<?php echo $nombre; ?>">
+	          <input type="text" id="nombre_alumno" class="form-control" placeholder="Nombre" value="<?php echo  htmlspecialchars($nombre,  ENT_QUOTES, 'UTF-8'); ?>">
 	        </div>
 	      </div>
 	      <div class="col-md-4 pl-1">
 	        <div class="form-group">
 	          <label for="exampleInputEmail1">Apellido Paterno</label>
-	          <input type="text" id="apellido_paterno" class="form-control" placeholder="Paterno" value="<?php echo $apellido_paterno; ?>">
+	          <input type="text" id="apellido_paterno" class="form-control" placeholder="Paterno" value="<?php echo htmlspecialchars($apellido_paterno,  ENT_QUOTES, 'UTF-8') ; ?>">
 	        </div>
 	      </div>
 	      <div class="col-md-4 pl-1">
 	        <div class="form-group">
 	          <label for="exampleInputEmail1">Apellido Materno</label>
-	          <input type="text" id="apellido_materno" class="form-control" placeholder="Materno" value="<?php echo $apellido_materno; ?>">
+	          <input type="text" id="apellido_materno" class="form-control" placeholder="Materno" value="<?php echo  htmlspecialchars($apellido_materno,  ENT_QUOTES, 'UTF-8'); ?>">
 	        </div>
 	      </div>
 	    </div>
 	    <div class="row">
 	      <div class="col-md-4">
 	        <label for="correo">Correo Electronico</label>
-	        <input id="correo" type="mail" id="correo" class="form-control" placeholder="Correo Electronico" value="<?php echo $correo; ?>">
+	        <input id="correo" type="mail" id="correo" class="form-control" placeholder="Correo Electronico" value="<?php echo  htmlspecialchars($correo,  ENT_QUOTES, 'UTF-8'); ?>">
 	      </div>
 	      <div class="col-md-4">
 	        <label for="fec_nac">Fecha de Nacimiento</label>
-	        <input id="fec_nac" type="date" id="fec_nac" class="form-control" placeholder="Fecha de nacimiento" value="<?php echo $fecha_naciemiento; ?>">
+	        <input id="fec_nac" type="date" id="fec_nac" class="form-control" placeholder="Fecha de nacimiento" value="<?php echo  htmlspecialchars($fecha_naciemiento,  ENT_QUOTES, 'UTF-8'); ?>">
 	      </div>
 	      <div class="col-md-4">
 	        <label for="sexo">Sexo</label>
@@ -64,13 +64,13 @@ $id_alumno = $row_alumno[0]['id_alumno'];
 	        <div class="form-group">
 	          <label>Matricula</label>
 	          <input type="number" id="matricula" class="form-control" placeholder="Matricula" value="<?php echo
-	          $matricula;  ?>">
+	           htmlspecialchars($matricula,  ENT_QUOTES, 'UTF-8');  ?>">
 	        </div>
 	      </div>
 	      <div class="col-md-6 pl-1">
 	        <div class="form-group">
 	          <label>CURP</label>
-	          <input type="text" id="curp" class="form-control" placeholder="CURP" value="<?php echo $curp;?>">
+	          <input type="text" id="curp" class="form-control" placeholder="CURP" value="<?php echo  htmlspecialchars($curp,  ENT_QUOTES, 'UTF-8');?>">
 	        </div>
 	      </div>
 	    </div>
@@ -78,13 +78,13 @@ $id_alumno = $row_alumno[0]['id_alumno'];
 	      <div class="col-md-6">
 	        <div class="form-group">
 	          <label>Telefono Celular</label>
-	          <input type="number" id="tel_celular" class="form-control" placeholder="Telefono Celular" value="<?php echo $telefono_celular; ?>">
+	          <input type="number" id="tel_celular" class="form-control" placeholder="Telefono Celular" value="<?php echo  htmlspecialchars($telefono_celular,  ENT_QUOTES, 'UTF-8'); ?>">
 	        </div>
 	      </div>
 	      <div class="col-md-6">
 	        <div class="form-group">
 	          <label>Telefono Casa</label>
-	          <input type="number" id="tel_casa" class="form-control" placeholder="Telefono Casa" value="<?php echo $telefono_casa ?>">
+	          <input type="number" id="tel_casa" class="form-control" placeholder="Telefono Casa" value="<?php echo  htmlspecialchars($telefono_casa,  ENT_QUOTES, 'UTF-8'); ?>">
 	        </div>
 	      </div>
 	    </div>
@@ -92,7 +92,7 @@ $id_alumno = $row_alumno[0]['id_alumno'];
 	    	<div class="col-md-12">
 		        <div class="form-group">
 		          <label>Direccion</label>
-		          <input type="text" id="direccion" class="form-control" placeholder="Calle Numero Colonia, Municipio" value="<?php echo $direccion ?>">
+		          <input type="text" id="direccion" class="form-control" placeholder="Calle Numero Colonia, Municipio" value="<?php echo  htmlspecialchars($direccion,  ENT_QUOTES, 'UTF-8'); ?>">
 		        </div>
 	      	</div>
 	    </div>
@@ -118,7 +118,7 @@ $id_alumno = $row_alumno[0]['id_alumno'];
 	    </div>
 	  </div>
 	  <div class="card-footer">
-	    <a href="#!" class="btn btn-primary btn-block" id="btn_modificar_alumno" data-id="<?php echo $id_alumno ?>">Modificar Alumno</a>
+	    <a href="#!" class="btn btn-primary btn-block" id="btn_modificar_alumno" data-id="<?php echo  htmlspecialchars($id_alumno,  ENT_QUOTES, 'UTF-8'); ?>">Modificar Alumno</a>
 	  </div>
 	</div>
 </div>
